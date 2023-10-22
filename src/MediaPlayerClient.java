249a871225f6108989c3ac3af027c8355ce3d8a5
AlikhanSagatbekov <221112@astana.edu.kz>
2023-10-22 23:32:04 +0600

    100%

--- src/MediaPlayerClient.java
+++ src/MediaPlayerClient.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
+
 public class MediaPlayerClient {
     public static void main(String[] args) {
         // Create an AudioPlayer and a basic Speaker
         AudioPlayer audioPlayer = new SimpleAudioPlayer();
         Speaker basicSpeaker = new BasicSpeaker();
 
-        // Play audio using the AudioPlayer directly
-        audioPlayer.playAudio("MP3", "song.mp3");
+        Scanner scanner = new Scanner(System.in);
 
-        // Play audio using the basic Speaker with the Adapter
-        AudioPlayer speakerAdapter = new SpeakerAdapter(basicSpeaker, "FLAC");
-        speakerAdapter.playAudio("FLAC", "music.flac");
+        System.out.println("Select an audio format (MP3 or FLAC):");
+        String selectedFormat = scanner.next();
+
+        // Ask the user for the song's file name
+        System.out.println("Enter the song file name:");
+        String songFileName = scanner.next();
+
+        // Play audio using the selected format and the AudioPlayer or the basic Speaker with the Adapter
+        if ("MP3".equalsIgnoreCase(selectedFormat)) {
+            audioPlayer.playAudio("MP3", songFileName);
+        } else if ("FLAC".equalsIgnoreCase(selectedFormat)) {
+            AudioPlayer speakerAdapter = new SpeakerAdapter(basicSpeaker, "FLAC");
+            speakerAdapter.playAudio("FLAC", songFileName);
+        } else {
+            System.out.println("Unsupported audio format.");
+        }
     }
 }
